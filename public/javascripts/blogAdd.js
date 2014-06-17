660542481a59c2d024d6822344bc869fd2c63080
ycliyang <lyg_hi@163.com>
2014-06-17 18:32:51 +0800

    2
    2

--- public/javascripts/blogAdd.js
+++ public/javascripts/blogAdd.js
@@ -1,7 +1,12 @@
 /**
  * Created by Administrator on 2014/6/16.
  */
-$(function (){
-    alert(1)
-    $('#editor').wysiwyg();
-})();
+$(document).ready(function (){
+
+
+
+});
+
+function blogAddSubmit(){
+
+}
